92bd31175f362d6670abd5bdee6355188afcd141
nandarajiv <nanda.variar@gmail.com>
2024-02-24 16:15:00 +0530

    added some methods for oms need to make changes for portfolio_value depending on StockExchange - it has to query the StockExchange for current price in order to get portfolio value may also update sell_stock method once it is done

--- order_management_system.py
+++ order_management_system.py
@@ -6,5 +6,41 @@
 # e. A method to place sell orders.
 
 class OrderManagementSystem:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, trader_name, portfolio, account_balance = 0, portfolio_value = 0):
+        self.trader_name = trader_name
+        self.account_balance = account_balance
+        self.portfolio = portfolio # list of dictionaries, where each dictionary is stock_name, quantity, buy_price
+        self.portfolio_value = portfolio_value
+
+    def get_balance(self):
+        return self.account_balance
+    
+    def get_portfolio_value(self):
+        return self.portfolio_value
+    
+    def show_portfolio(self):
+        for stock in self.portfolio:
+            print(stock)
+        return self.portfolio
+    
+    def buy_stock(self, stock_name, quantity, buy_price):
+        self.account_balance -= (quantity * buy_price)
+        self.portfolio.append({'stock_name': stock_name, 'quantity': quantity, 'buy_price': buy_price})
+
+    def sell_stock(self, stock_name, quantity, sell_price):
+        found = 0
+        for stock_dict in self.portfolio:
+            if stock_dict['stock_name'] == stock_name:
+                found = 1
+                if stock_dict['quantity'] >= quantity:
+                    stock_dict['quantity'] -= quantity
+                    self.account_balance += (quantity * sell_price)
+                    if stock_dict['quantity'] == 0:
+                        sold_stock = stock_dict
+                        self.portfolio.remove(stock_dict)
+
+        if found == 0:
+            print('Stock not found in portfolio')
+            sold_stock = None
+
+        return sold_stock
